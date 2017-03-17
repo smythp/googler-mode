c6424111476bfca8bc9a82afd283bfcad8bb98d2
Patrick Smyth <patricksmyth01@gmail.com>
2017-03-17 17:33:57 -0400

    added functions to move through results

--- googler-mode.el
+++ googler-mode.el
@@ -19,26 +19,43 @@
 	(abstract (cdr (assoc 'abstract search-entry))))
     (progn 
       (insert-hyperlink url title)
-      (insert "\n\n" abstract "\n\n"))))
+      (insert "\n\n" abstract "\n\n")
+      ;; return position shift after insert
+      ;; for user ability to move between results
+      (+ (length title) (length abstract) 4))))
 
 
-(defun render-all-entries (sequence)
-  (seq-map 'render-search-entry sequence))
+(defun googler-cumulative-list (in-list carry)
+  "Make each value of IN-LIST the cumulative total of that value and all previous values."
+  (if in-list
+       (cons
+	(+ (car in-list) carry)
+	(googler-cumulative-list (cdr in-list) (+ carry (car in-list))))))
 
 
-(defun googler-results-buffer (query)
-  (let ((results (googler-get-results query)))
-    (progn 
-      (get-buffer-create "*googler-results*")
-      (with-current-buffer "*googler-results*"
-	(let ((buffer-read-only nil))
-	  (progn
-	    (erase-buffer)
-	    (insert "Results for " query "\n\n")
-	    (render-all-entries results)
-	    (read-only-mode)
-	    (goto-char (point-min)))))
-      (switch-to-buffer "*googler-results*"))))
+(defun render-all-entries (vector offset)
+  "Inserts text for results from VECTOR as a side effect. Returns the buffer locations of the beginning of each entry as a list so that user can move between them. Offset is used to shift results by the length of any preamble at the beginnig of buffer."
+
+  (googler-cumulative-list
+  ;; put offset at beginning of list with cons
+  (cons offset
+	(seq-map 'render-search-entry vector)) 0))
+
+
+  (defun googler-results-buffer (query)
+    "Update the results buffer based on a QUERY."
+    (let ((results (googler-get-results query)))
+      (progn 
+	(get-buffer-create "*googler-results*")
+	(with-current-buffer "*googler-results*"
+	  (let ((buffer-read-only nil))
+	    (progn
+	      (erase-buffer)
+	      (insert "Results for " query "\n\n")
+	      (setq googler-results-locations (render-all-entries results (+ (length query) 15)))
+	      (read-only-mode)
+	      (goto-char (point-min)))))
+	(switch-to-buffer "*googler-results*"))))
 
 
 (defun googler-search (&optional query)
@@ -50,3 +67,36 @@
      (read-from-minibuffer "Googler search: "))))
 
 
+(defun googler-next ()
+  "Move point to next result while on the Googler results page."
+  (interactive)
+  (goto-char
+   (car (delq nil
+	      (mapcar (lambda (x) (if (< (point) x) x))
+		      googler-results-locations)))))
+
+
+(defun googler-next ()
+  "Move point to next result while on the Googler results page."
+  (interactive)
+  (goto-char
+   (car (delq nil
+	      (mapcar (lambda (x) (if (> (point) x) x))
+		      googler-results-locations)))))
+
+
+(defun googler-previous ()
+  "Move point to next result while on the Googler results page."
+  (interactive)
+  (goto-char
+   (car (last (delq nil
+	      (mapcar (lambda (x) (if (> (point) x) x))
+		      googler-results-locations))))))
+
+
+;; (defun googler-move (&optional back)
+;;   "Move forward or back in the Googler results page. If optional BACK is non-nil, moves back, else moves forward."
+;;   (let ((valid-locations (delq nil (mapcar (lambda (x) (if (< (point) x) x)) googler-results-locations))))
+;;     (goto-char
+;;        (car valid-locations))))
+;; ;	 (car (last valid-locations))
